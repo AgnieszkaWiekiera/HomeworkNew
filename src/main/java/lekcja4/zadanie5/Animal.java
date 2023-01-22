c4efd033ad78f0a23762defb4db7ad85a23c3664
Agnieszka Wiekiera <agnieszka.wiekiera97@gmail.com>
2023-01-22 14:58:39 +0100

    lekcja 4 zadanie5

---
+++ src/main/java/lekcja4/zadanie5/Animal.java
@@ -0,0 +1,8 @@
+package lekcja4.zadanie5;
+
+public class Animal {
+    protected String name;
+    public Animal(String name){
+        this.name=name;
+    }
+}
---
+++ src/main/java/lekcja4/zadanie5/Cat.java
@@ -0,0 +1,8 @@
+package lekcja4.zadanie5;
+
+public class Cat extends Animal{
+
+    public Cat(String name) {
+        super(name);
+    }
+}
