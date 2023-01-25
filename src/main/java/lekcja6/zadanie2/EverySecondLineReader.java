70d52a766820b16f375b5c048b063647c095e1ba
Agnieszka Wiekiera <agnieszka.wiekiera97@gmail.com>
2023-01-25 22:35:50 +0100

    lekcja 6 zadanie 2

--- src/main/java/lekcja6/zadanie2/EverySecondLineReader.java
+++ src/main/java/lekcja6/zadanie2/EverySecondLineReader.java
@@ -6,29 +6,22 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class EverySecondLineReader {
-    private String filePath;
 
-    public EverySecondLineReader(String filePath) {
-        this.filePath = filePath;
-    }
-
-    public void readEveryOtherLine() throws IOException {
-        File file = new File(filePath);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-
-        String st;
-        int lineNumber = 1;
-        while ((st = br.readLine()) != null) {
-            if (lineNumber % 2 == 0) {
-                System.out.println(st);
+    public static void read(String name) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(name))) {
+            String line;
+            int lineNumber = 0;
+            while ((line = reader.readLine()) != null) {
+                lineNumber++;
+                if (lineNumber % 2 == 0) {
+                    System.out.println(line);
+                }
             }
-            lineNumber++;
+        } catch (IOException e) {
+            System.err.println("Podczas odczytu pliku wystąpił błąd: " + e.getMessage());
         }
-        br.close();
     }
-
     public static void main(String[] args) throws IOException {
-        EverySecondLineReader reader = new EverySecondLineReader("data.txt");
-        reader.readEveryOtherLine();
+       read("data.txt");
     }
 }
