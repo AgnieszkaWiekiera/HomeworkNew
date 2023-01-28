e0900c13fea1608fa535d92703d5746da832e28b
Agnieszka Wiekiera <agnieszka.wiekiera97@gmail.com>
2023-01-28 18:05:20 +0100

    lekcja 8 zadanie 2

---
+++ src/main/java/lekcja8/zadanie2/OwnArrayList.java
@@ -0,0 +1,64 @@
+package lekcja8.zadanie2;
+
+import java.util.Arrays;
+
+public class OwnArrayList implements OwnList{
+    private Integer[] array;
+    private int size;
+
+    public OwnArrayList() {
+        array = new Integer[10];
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(Integer element) {
+        ensureCapacity();
+        array[size] = element;
+        size++;
+    }
+
+    public Integer get(int i) {
+        if (i < 0 || i >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return array[i];
+    }
+
+    public void add(int index, Integer element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        ensureCapacity();
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = element;
+        size++;
+    }
+
+    public Integer remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Integer removed = array[index];
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+        return removed;
+    }
+
+    private void ensureCapacity() {
+        if (size == array.length) {
+            array = Arrays.copyOf(array, array.length * 2);
+        }
+    }
+}
