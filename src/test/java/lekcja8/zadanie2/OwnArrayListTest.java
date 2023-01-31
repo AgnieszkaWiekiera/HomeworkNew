fdf3e6e87763ef4f78c394f4da5104a39cf24a17
Agnieszka Wiekiera <agnieszka.wiekiera97@gmail.com>
2023-01-31 23:31:10 +0100

    lekcja 8 zadanie 2 testy

--- src/test/java/lekcja8/zadanie2/OwnArrayListTest.java
+++ src/test/java/lekcja8/zadanie2/OwnArrayListTest.java
@@ -1,68 +1,72 @@
 package lekcja8.zadanie2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class OwnArrayListTest {
-
-    private OwnArrayList list = new OwnArrayList();
-
     @Test
     void shouldGetListSize() {
+        OwnArrayList list = new OwnArrayList();
         list.add(9);
         list.add(8);
-        assertEquals(2, list.size());
+        Assertions.assertEquals(2, list.size());
     }
 
     @Test
     void shouldGetIsEmptyTrue() {
-        assertTrue(list.isEmpty());
+        OwnArrayList list = new OwnArrayList();
+        Assertions.assertTrue(list.isEmpty());
     }
 
     @Test
     void shouldGetIsEmptyFalse() {
+        OwnArrayList list = new OwnArrayList();
         list.add(5);
-        assertFalse(list.isEmpty());
+        Assertions.assertFalse(list.isEmpty());
     }
 
     @Test
     void shouldAdd() {
+        OwnArrayList list = new OwnArrayList();
         list.add(1);
         list.add(2);
-        assertEquals(1, list.get(0));
-        assertEquals(2, list.get(1));
+        Assertions.assertEquals(1, list.get(0));
+        Assertions.assertEquals(2, list.get(1));
     }
 
     @Test
     void shouldGetAtIndex() {
+        OwnArrayList list = new OwnArrayList();
         list.add(1);
         list.add(2);
-        assertEquals(1, list.get(0));
-        assertEquals(2, list.get(1));
+        Assertions.assertEquals(1, list.get(0));
+        Assertions.assertEquals(2, list.get(1));
     }
 
     @Test
     void shouldAddWithIndex() {
+        OwnArrayList list = new OwnArrayList();
         list.add(0, 1);
         list.add(1, 2);
         list.add(2, 3);
-        assertEquals(1, list.get(0));
-        assertEquals(2, list.get(1));
-        assertEquals(3, list.get(2));
+        Assertions.assertEquals(1, list.get(0));
+        Assertions.assertEquals(2, list.get(1));
+        Assertions.assertEquals(3, list.get(2));
     }
 
     @Test
     void shouldRemoveFromList() {
+        OwnArrayList list = new OwnArrayList();
         list.add(4);
         list.add(6);
         list.remove(1);
-        assertEquals(4, list.get(0));
-        assertEquals(1, list.size());
+        Assertions.assertEquals(4, list.get(0));
+        Assertions.assertEquals(1, list.size());
     }
 
     @Test
     void shouldRemoveOutOfBounds() {
-        assertThrows(IndexOutOfBoundsException.class, () -> list.remove(0));
+        OwnArrayList list = new OwnArrayList();
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> list.remove(0));
     }
 }
