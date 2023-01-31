727db6338b79ce79a8a1bda7824b53471be6869d
Agnieszka Wiekiera <agnieszka.wiekiera97@gmail.com>
2023-01-31 22:23:51 +0100

    lekcja 10 zadanie 2

--- src/test/java/lekcja10/zadanie2pesel/PeselValidatorTest.java
+++ src/test/java/lekcja10/zadanie2pesel/PeselValidatorTest.java
@@ -1,6 +1,5 @@
 package lekcja10.zadanie2pesel;
 
-import lekcja10.zadanie2.PeselValidatorGorszy;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,21 +9,21 @@ class PeselValidatorTest {
     @Test
     void isPeselCorrect() {
         String pesel = "44051401458";
-        boolean isPesel = PeselValidatorGorszy.isPesel(pesel);
+        boolean isPesel = PeselValidator.isPesel(pesel);
         Assertions.assertTrue(isPesel);
     }
 
     @Test
     void isPeselIncorrectLength() {
         String pesel = "05140145";
-        boolean isPesel = PeselValidatorGorszy.isPesel(pesel);
+        boolean isPesel = PeselValidator.isPesel(pesel);
         Assertions.assertFalse(isPesel);
     }
 
     @Test
     void isPeselIncorrectChar() {
         String pesel = "4b05140145A";
-        boolean isPesel = PeselValidatorGorszy.isPesel(pesel);
+        boolean isPesel = PeselValidator.isPesel(pesel);
         Assertions.assertFalse(isPesel);
     }
 }
