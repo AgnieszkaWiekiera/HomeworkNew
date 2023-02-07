aa65414fb758fe8b733aa268ae6bb11c122e280a
Agnieszka Wiekiera <agnieszka.wiekiera97@gmail.com>
2023-02-07 17:51:00 +0100

    lekcja 10 zadanie 5 testy

--- src/test/java/lekcja10/zadanie5/PasswordValidatorTest.java
+++ src/test/java/lekcja10/zadanie5/PasswordValidatorTest.java
@@ -8,31 +8,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class PasswordValidatorTest {
 
     @Test
-    void isPasswordCorrect() {
+    void shouldPasswordBeCorrect() {
         String password = "Historia44";
         boolean isValid = PasswordValidator.isValid(password);
         Assertions.assertTrue(isValid);
     }
 
     @Test
-    void isPasswordIncorrectWithoutNumber() {
+    void shouldPasswordBeIncorrectWithoutNumber() {
         String password = "Historia";
         boolean isValid = PasswordValidator.isValid(password);
         Assertions.assertFalse(isValid);
     }
 
     @Test
-    void isPasswordIncorrectWithoutBigFont() {
+    void shouldPasswordBeIncorrectWithoutBigFont() {
         String password = "historia44";
         boolean isValid = PasswordValidator.isValid(password);
         Assertions.assertFalse(isValid);
     }
 
     @Test
-    void isPasswordIncorrectWithActualYear() {
+    void shouldPasswordBeIncorrectWithActualYear() {
         String password = "Historia23";
         boolean isValid = PasswordValidator.isValid(password);
         Assertions.assertFalse(isValid);
     }
 
+    @Test
+    void shouldPasswordBeIncorrectWithLessCharts() {
+        String password = "Hist44";
+        boolean isValid = PasswordValidator.isValid(password);
+        Assertions.assertFalse(isValid);
+    }
 }
