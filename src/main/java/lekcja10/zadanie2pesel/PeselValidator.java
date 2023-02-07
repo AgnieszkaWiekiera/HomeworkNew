7eb9e9488678dcba183794fa60ecd202ebfe9791
Agnieszka Wiekiera <agnieszka.wiekiera97@gmail.com>
2023-02-07 17:43:10 +0100

    lekcja 10 zadanie 2 ze zmianami

--- src/main/java/lekcja10/zadanie2pesel/PeselValidator.java
+++ src/main/java/lekcja10/zadanie2pesel/PeselValidator.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PeselValidator {
-    public static boolean isPesel(String pesel){
+    public static boolean isValid(String pesel){
         Pattern pattern = Pattern.compile("^\\d{11}");
         Matcher matcher = pattern.matcher(pesel);
         return matcher.matches();
